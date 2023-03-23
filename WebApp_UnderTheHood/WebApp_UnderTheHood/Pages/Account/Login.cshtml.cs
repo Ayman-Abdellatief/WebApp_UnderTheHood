a3e34bdae2b9ebc58642d21c9700e56e9f1eb7e7
ayman.abdellatief <ayman.abdellatief@it-blocks.com>
2023-03-24 01:18:39 +0200

    Create Login Page

---
+++ WebApp_UnderTheHood/WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp_UnderTheHood.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credintial credintial { get; set; }
+
+        public void OnGet()
+        {
+      
+        }
+
+        public void OnPost()
+        {
+
+        }
+
+    }
+
+
+    public class Credintial
+    {
+        [Required]
+        [Display(Name ="User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+    }
+}
