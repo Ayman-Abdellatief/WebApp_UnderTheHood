a5b0440087fb4d069ee7bcc52b53fc21fff8a16d
ayman.abdellatief <ayman.abdellatief@it-blocks.com>
2023-03-24 04:29:38 +0200

    Add authintecations claims

--- WebApp_UnderTheHood/WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
+++ WebApp_UnderTheHood/WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -18,9 +20,27 @@ namespace WebApp_UnderTheHood.Pages.Account
       
         }
 
-        public void OnPost()
+        public async Task<IActionResult>  OnPost()
         {
+            if (!ModelState.IsValid) return Page();
+            //Verify Credintial
+            if(credintial.UserName=="admin" && credintial.Password=="P@ssw0rd")
+            {
+                // Create the Security Context
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,"admin"),
+                    new Claim(ClaimTypes.Email,"Ay@ayman.com")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
 
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
         }
 
     }
